0d6848df0fe11c151968f2ce35a02e87e843e05e
Peter Hellberg <peter@c7.se>
2016-12-12 14:26:57 +0100

    Added subtest for nil data

--- features_test.go
+++ features_test.go
@@ -3,32 +3,54 @@ package features
 import "testing"
 
 func TestNew(t *testing.T) {
-	f := New(map[string]bool{
-		"foo": true,
-		"bar": false,
+	t.Run("nil", func(t *testing.T) {
+		f := New(nil)
+
+		if f.Enabled("foo") == true {
+			t.Fatalf(`f.Enabled("foo") == true, want false`)
+		}
+
+		f.Enable("foo")
+
+		if f.Enabled("foo") == false {
+			t.Fatalf(`f.Enabled("foo") == false, want true`)
+		}
+
+		f.Disable("foo")
+
+		if f.Enabled("foo") == true {
+			t.Fatalf(`f.Enabled("foo") == true, want false`)
+		}
 	})
 
-	if f.Enabled("foo") == false {
-		t.Fatalf(`f.Enabled("foo") == false, want true`)
-	}
+	t.Run("normal", func(t *testing.T) {
+		f := New(map[string]bool{
+			"foo": true,
+			"bar": false,
+		})
 
-	if f.Enabled("bar") == true {
-		t.Fatalf(`f.Enabled("bar") == true, want false`)
-	}
+		if f.Enabled("foo") == false {
+			t.Fatalf(`f.Enabled("foo") == false, want true`)
+		}
 
-	if f.Enabled("baz") == true {
-		t.Fatalf(`f.Enabled("baz") == true, want false`)
-	}
+		if f.Enabled("bar") == true {
+			t.Fatalf(`f.Enabled("bar") == true, want false`)
+		}
 
-	f.Enable("baz")
+		if f.Enabled("baz") == true {
+			t.Fatalf(`f.Enabled("baz") == true, want false`)
+		}
 
-	if f.Enabled("baz") == false {
-		t.Fatalf(`f.Enabled("baz") == false, want true`)
-	}
+		f.Enable("baz")
 
-	f.Disable("baz")
+		if f.Enabled("baz") == false {
+			t.Fatalf(`f.Enabled("baz") == false, want true`)
+		}
 
-	if f.Enabled("baz") == true {
-		t.Fatalf(`f.Enabled("baz") == true, want false`)
-	}
+		f.Disable("baz")
+
+		if f.Enabled("baz") == true {
+			t.Fatalf(`f.Enabled("baz") == true, want false`)
+		}
+	})
 }
